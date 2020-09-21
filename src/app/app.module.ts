c4110cdc878bdeb51b0ad0fd5b89f999c468b7db
Dhruv <dgupta@muellerwp.com>
2020-09-21 11:45:42 -0400

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 
 @NgModule({    
     imports: [BrowserModule],
-    declarations:[AppComponent], 
+    declarations:[AppComponent, MediaItemComponent], 
     bootstrap: [AppComponent]
 })
 
